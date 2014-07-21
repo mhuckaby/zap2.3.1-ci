fcd4aeb1629fdd7c3213060e8f88e18193c6280a
mhuckaby <mhuckaby@gmail.com>
2014-07-21 15:49:55 -0500

    added output for params

--- generateHtmlOutput.groovy
+++ generateHtmlOutput.groovy
@@ -8,4 +8,9 @@ import javax.xml.transform.stream.StreamSource;
 import java.io.File;
 import static org.parosproxy.paros.extension.report.ReportGenerator.fileToHtml;
 
+
+println "xml file to convert ${args[0]}" 
+println "xsl file to use in conversion ${args[1]}"
+println "html file to write ${args[2]}"
+
 fileToHtml(args[0], args[1], args[2]);
